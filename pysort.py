e9fee124caa5723b50e6305ec97e79fc0ac20956
Luke <luke@MacBook-Air.local>
2020-02-27 10:18:05 -0600

    implemented manual sorting algorithm

--- pysort.py
+++ pysort.py
@@ -1,14 +1,38 @@
 import random
 
 random_array = [] #initialize empty array to hold random values
-int random
+number = 0 # holds each randomly generated value
+
 for x in range(10):
-  random = random.randint(1,101) #10 random values of numbers between 1 and 100
+  number = random.randint(1,101) #10 random values of numbers between 1 and 100
   print random
-  random_array.append(random)
+  random_array.append(number)
 
 # display original random array
-for x in random_array
+######
+print "Random Array"
+for x in random_array:
     print x
 
-#
+lowest = 100
+index = 0 ## index of lowest
+x_index = 0
+y_index = 0
+for x in random_array:
+    for y in random_array:
+        if(y_index < x_index):
+            y_index += 1
+            continue
+        if(y < lowest):
+            lowest = y
+            index = y_index
+        y_index += 1
+    random_array[index] = random_array[x_index]
+    random_array[x_index] = lowest
+    x_index += 1
+    lowest = 100
+
+# display sorted array
+print "Sorted Array"
+for x in random_array:
+    print x
