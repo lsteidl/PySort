a1fee52f7973174556e4dcdcd392a233d574d587
Luke <luke@MacBook-Air.local>
2020-02-27 09:57:29 -0600

    generating random array

---
+++ pysort.py
@@ -0,0 +1,14 @@
+import random
+
+random_array = [] #initialize empty array to hold random values
+int random
+for x in range(10):
+  random = random.randint(1,101) #10 random values of numbers between 1 and 100
+  print random
+  random_array.append(random)
+
+# display original random array
+for x in random_array
+    print x
+
+#
